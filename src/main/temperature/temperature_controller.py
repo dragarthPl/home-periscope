12dafb8adb286a2794089221bb81d1420968c340
Marcin Goncerzewicz <marcingoncerzewicz@gmail.com>
2024-03-11 07:52:03 +0100

    [ENH] water heater temperature

--- src/main/temperature/temperature_controller.py
+++ src/main/temperature/temperature_controller.py
@@ -7,6 +7,7 @@ from fastapi_utils.inferring_router import InferringRouter
 
 from main.temperature.heating_temperature_repository import HeatingTemperatureRepository
 from main.temperature.temperature import Temperature
+from main.temperature.water_heater_temperature_repository import WaterHeaterTemperatureRepository
 
 temperature_router = InferringRouter(tags=["DashboardController"])
 
@@ -14,11 +15,23 @@ temperature_router = InferringRouter(tags=["DashboardController"])
 @cbv(temperature_router)
 class TemperatureController:
     heating_temperature_repository: HeatingTemperatureRepository = Injected(HeatingTemperatureRepository)
-
+    water_heater_temperature_repository: WaterHeaterTemperatureRepository = Injected(WaterHeaterTemperatureRepository)
 
     @temperature_router.get("/api/heating_temperature")
     async def heating_temperature(self, request: Request):
         temperature: Temperature = await self.heating_temperature_repository.get_temperature()
+        if temperature:
+            return asdict(temperature)
+        else:
+            return {
+                "current": None,
+                "max_temperature": None,
+                "min_temperature": None,
+            }
+
+    @temperature_router.get("/api/water_heater_temperature")
+    async def water_heater_temperature(self, request: Request):
+        temperature: Temperature = await self.water_heater_temperature_repository.get_temperature()
         if temperature:
             return asdict(temperature)
         else:

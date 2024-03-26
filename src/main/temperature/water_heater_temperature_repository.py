3ea922fbcecd48b1b990d98740e86aa316b58276
Marcin Goncerzewicz <marcingoncerzewicz@gmail.com>
2024-03-26 07:58:52 +0100

    [ENH] Water heater temperature - fix for backend

--- src/main/temperature/water_heater_temperature_repository.py
+++ src/main/temperature/water_heater_temperature_repository.py
@@ -21,13 +21,15 @@ class WaterHeaterTemperatureRepository(IWaterHeaterTemperatureRepository):
     def __init__(self, configuration: Configuration):
         self.stream_ip = configuration.stream_ip
         self.stream_port = configuration.stream_port
-    # split-environments
+
     async def get_temperature(self) -> Temperature:
         async with pyplumio.open_tcp_connection(self.stream_ip, self.stream_port) as conn:
             ecomax = await conn.get("ecomax")
             heating_target_temp = await ecomax.get("water_heater_target_temp")
+            water_heater_temp = await ecomax.get("water_heater_temp")
             return Temperature.from_dict({
                 'max_temperature': heating_target_temp.max_value,
                 'min_temperature': heating_target_temp.min_value,
-                'current': heating_target_temp.value,
+                'target_temperature': heating_target_temp.value,
+                'current': int(water_heater_temp),
             })

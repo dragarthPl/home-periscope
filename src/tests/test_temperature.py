0706485bf6ba2c5bad6d8e7fe4e473e590a7cfbd
Marcin Goncerzewicz <marcingoncerzewicz@gmail.com>
2024-03-28 08:02:27 +0100

    [ENH] Update test

---
+++ src/tests/test_temperature.py
@@ -0,0 +1,22 @@
+import unittest
+import asyncio
+from fastapi.testclient import TestClient
+
+from main.core.sample_container import sample_container
+from main.home_periscope import HomePeriscope
+
+client = TestClient(HomePeriscope.create_app(sample_container))
+loop = asyncio.get_event_loop()
+
+
+class TestTemperature(unittest.TestCase):
+
+    def test_heating_temperature(self) -> None:
+        response = client.get("/api/heating_temperature")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual({
+            'current': 30,
+            'max_temperature': 60,
+            'min_temperature': 20,
+            'target_temperature': 40,
+        }, response.json())

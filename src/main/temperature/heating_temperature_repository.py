1e10e54c49a6f167b9fa3f6a55f4bde8ca0e4e1f
Marcin Goncerzewicz <marcingoncerzewicz@gmail.com>
2024-04-08 22:01:34 +0200

    [ENH] Heating temperature populated from redis

--- src/main/temperature/heating_temperature_repository.py
+++ src/main/temperature/heating_temperature_repository.py
@@ -2,6 +2,7 @@ from abc import ABC, abstractmethod
 
 import pyplumio
 from injector import inject
+from redis import Redis
 
 from main.core.configuration import Configuration
 from main.temperature.temperature import Temperature
@@ -20,23 +21,23 @@ class IHeatingTemperatureRepository(ABC):
 class HeatingTemperatureRepository(IHeatingTemperatureRepository):
     stream_ip: str
     stream_port: int
+    __redis: Redis
 
     @inject
-    def __init__(self, configuration: Configuration):
+    def __init__(self, configuration: Configuration, redis: Redis):
         self.stream_ip = configuration.stream_ip
         self.stream_port = configuration.stream_port
+        self.__redis = redis
 
     async def get_temperature(self) -> Temperature:
-        async with pyplumio.open_tcp_connection(self.stream_ip, self.stream_port) as conn:
-            ecomax = await conn.get("ecomax")
-            heating_target_temp = await ecomax.get("heating_target_temp")
-            heating_temp = await ecomax.get("heating_temp")
-            return Temperature.from_dict({
-                'max_temperature': heating_target_temp.max_value,
-                'min_temperature': heating_target_temp.min_value,
-                'target_temperature': heating_target_temp.value,
-                'current': int(heating_temp),
-            })
+        stove_data = self.__redis.hgetall("stove_data")
+        return Temperature.from_dict({
+            'max_temperature': int(stove_data.get("max_heating_target_temp")),
+            'min_temperature': int(stove_data.get("min_heating_target_temp")),
+            'target_temperature': int(stove_data.get("heating_target_temp")),
+            'current': int(float(stove_data.get("heating_temp"))),
+            'timestamp': int(float(stove_data.get("timestamp"))),
+        })
 
     async def set_temperature(self, temperature: int) -> bool:
         async with pyplumio.open_tcp_connection(self.stream_ip, self.stream_port) as conn:

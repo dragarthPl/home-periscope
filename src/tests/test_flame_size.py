d29327d67edd43c2421ecd55f6983c1936a8b154
Marcin Goncerzewicz <marcingoncerzewicz@gmail.com>
2024-04-18 08:00:07 +0200

    [ENH] Test for flame size

---
+++ src/tests/test_flame_size.py
@@ -0,0 +1,17 @@
+import asyncio
+import unittest
+
+from starlette.testclient import TestClient
+
+from main.core.sample_container import sample_container
+from main.home_periscope import HomePeriscope
+
+client = TestClient(HomePeriscope.create_app(sample_container))
+loop = asyncio.get_event_loop()
+
+
+class TestFlameSize(unittest.TestCase):
+    def test_flame_size(self):
+        response = client.get("/api/flame_size")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual({'flame_size': '30%'}, response.json())

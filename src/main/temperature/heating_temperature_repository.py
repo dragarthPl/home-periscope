6a12881a22d809fad48145293da49020605efe18
Marcin Goncerzewicz <marcingoncerzewicz@gmail.com>
2024-04-24 22:59:27 +0200

    [ENH] Update heating repository

--- src/main/temperature/heating_temperature_repository.py
+++ src/main/temperature/heating_temperature_repository.py
@@ -46,7 +46,7 @@ class HeatingTemperatureRepository(IHeatingTemperatureRepository):
             "value": temperature,
         }
         try:
-            self.__redis.lpush("stove_command", json.dumps(command))
+            self.__redis.publish("command-channel", json.dumps(command))
         except Exception:
             return False
         return True

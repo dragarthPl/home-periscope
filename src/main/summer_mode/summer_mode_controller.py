3c92de0dfffb9f38f3427213908cf1f433bfbb7f
Marcin Goncerzewicz <marcingoncerzewicz@gmail.com>
2024-05-07 22:51:24 +0200

    [ENH] Summer mode controller

---
+++ src/main/summer_mode/summer_mode_controller.py
@@ -0,0 +1,24 @@
+from fastapi import Request
+
+from fastapi_injector import Injected
+from fastapi_utils.cbv import cbv
+from fastapi_utils.inferring_router import InferringRouter
+
+from main.summer_mode.summer_mode import SummerMode
+
+from main.summer_mode.summer_mode_repository import ISummerModeRepository
+
+summer_mode_router = InferringRouter(tags=["SummerModeController"])
+
+
+@cbv(summer_mode_router)
+class SummerModeController:
+    summer_mode_repository: ISummerModeRepository = Injected(ISummerModeRepository)
+
+    @summer_mode_router.get("/api/summer_mode")
+    async def summer_mode(self, request: Request) -> str:
+        summer_mode: SummerMode = await self.summer_mode_repository.get_summer_mode()
+        if summer_mode:
+            return summer_mode.get_mode()
+        else:
+            return SummerMode.UNKNOWN

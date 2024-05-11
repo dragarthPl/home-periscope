cb0647a943cffe869e1dd27b1a0da3941ac1aaf1
Marcin Goncerzewicz <marcingoncerzewicz@gmail.com>
2024-05-11 23:35:06 +0200

    [ENH] Test for Summary mode

---
+++ src/tests/test_summary_mode.py
@@ -0,0 +1,17 @@
+import asyncio
+import unittest
+
+from starlette.testclient import TestClient
+
+from main.core.sample_container import sample_container
+from main.home_periscope import HomePeriscope
+
+client = TestClient(HomePeriscope.create_app(sample_container))
+loop = asyncio.get_event_loop()
+
+
+class TestSummaryMode(unittest.TestCase):
+    def test_summary_mode(self) -> None:
+        response = client.get("/api/summer_mode")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual('Lato', response.json())

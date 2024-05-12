ff8b48a658a00af400fa82609cf5f43b8181ab08
Marcin Goncerzewicz <marcingoncerzewicz@gmail.com>
2024-05-12 22:16:59 +0200

    [ENH] Update Summary mode model

--- src/main/summer_mode/summer_mode.py
+++ src/main/summer_mode/summer_mode.py
@@ -11,8 +11,19 @@ class SummerMode:
     def __init__(self, mode: int):
         self.__mode = mode
 
+    @classmethod
+    def from_mode_string(cls, mode: str) -> 'SummerMode':
+        if mode == cls.SUMMER:
+            return SummerMode(1)
+        elif mode == cls.WINTER:
+            return SummerMode(0)
+        else:
+            raise Exception("Unknown summer mode")
+
     def get_mode(self):
         if self.__mode == 1:
             return self.SUMMER
-        else:
+        elif self.__mode == 0:
             return self.WINTER
+        else:
+            raise Exception("Unknown summer mode")

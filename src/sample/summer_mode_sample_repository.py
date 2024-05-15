247fdf951bd28ce49a8b5bc3fe681c7f79db3e4a
Marcin Goncerzewicz <marcingoncerzewicz@gmail.com>
2024-05-15 06:36:13 +0200

    [ENH] Update Summary mode sample repository

--- src/sample/summer_mode_sample_repository.py
+++ src/sample/summer_mode_sample_repository.py
@@ -5,3 +5,6 @@ from main.summer_mode.summer_mode_repository import ISummerModeRepository
 class SummerModeSampleRepository(ISummerModeRepository):
     async def get_summer_mode(self) -> SummerMode:
         return SummerMode(1)
+
+    async def set_summer_mode(self, summer_mode: SummerMode) -> bool:
+        return True

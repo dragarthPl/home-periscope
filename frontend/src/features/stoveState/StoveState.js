13089ffd50a3df2cb176435d18cf216d803acc30
Marcin Goncerzewicz <marcingoncerzewicz@gmail.com>
2024-05-20 06:31:02 +0200

    [ENH] Stove state fix/changes

--- frontend/src/features/stoveState/StoveState.js
+++ frontend/src/features/stoveState/StoveState.js
@@ -1,7 +1,6 @@
 import CardContent from '@mui/material/CardContent';
 import Typography from "@mui/material/Typography";
 import {useSelector} from "react-redux";
-import {setHeatingTemperature} from "../temperature/temepratureSlice";
 
 const StoveState = () => {
     const stoveState = useSelector((state) => state.rootReducers.stoveState.stoveState)
@@ -18,19 +17,8 @@ const StoveState = () => {
     return (
         <CardContent>
           <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-              Stan pieca: {stoveState.stoveState}
+              <strong>Stan pieca: </strong> {stoveState.stoveState}
           </Typography>
-          {/*<Typography variant="h5" component="div">*/}
-          {/*  be nev o lent*/}
-          {/*</Typography>*/}
-          {/*<Typography sx={{ mb: 1.5 }} color="text.secondary">*/}
-          {/*  adjective*/}
-          {/*</Typography>*/}
-          {/*<Typography variant="body2">*/}
-          {/*  well meaning and kindly.*/}
-          {/*  <br />*/}
-          {/*  {'"a benevolent smile"'}*/}
-          {/*</Typography>*/}
         </CardContent>
     )
 }
--- frontend/src/features/stoveState/stoveStateSlice.js
+++ frontend/src/features/stoveState/stoveStateSlice.js
@@ -1,6 +1,5 @@
 import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 import {client} from "../../api/client";
-import {fetchHeatingTemperature} from "../temperature/temepratureSlice";
 
 const initialState = {
   stoveState: undefined,

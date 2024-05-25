ef6379f8dc6e9ef1edf0dd98a03bc8756b050197
Marcin Goncerzewicz <marcingoncerzewicz@gmail.com>
2024-05-25 23:26:28 +0200

    [ENH] reducers update

--- frontend/src/reducers.js
+++ frontend/src/reducers.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux'
 
 import temperatureReducer from './features/temperature/temepratureSlice'
 import stoveStateReducer from './features/stoveState/stoveStateSlice'
+import flameSizeReducer from './features/flameSize/flameSizeSlice'
 const rootReducer = combineReducers({
   temperature: temperatureReducer,
   stoveState: stoveStateReducer,
+  flameSize: flameSizeReducer,
 })
 
 export default rootReducer

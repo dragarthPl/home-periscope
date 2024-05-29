ffb967c1c61e5a193233d174e1472f2ee7dbd56e
Marcin Goncerzewicz <marcingoncerzewicz@gmail.com>
2024-05-30 00:07:48 +0200

    [ENH] summer mode controller update

--- src/main/summer_mode/summer_mode_controller.py
+++ src/main/summer_mode/summer_mode_controller.py
@@ -14,7 +14,7 @@ class SummerModeController:
     summer_mode_repository: ISummerModeRepository = Injected(ISummerModeRepository)
 
     @summer_mode_router.get("/api/summer_mode")
-    async def summer_mode(self) -> str:
+    async def summer_mode(self) -> dict[str, str]:
         summer_mode: SummerMode = await self.summer_mode_repository.get_summer_mode()
         if summer_mode:
             return summer_mode.get_mode()
@@ -22,7 +22,7 @@ class SummerModeController:
             return SummerMode.UNKNOWN
 
     @summer_mode_router.post("/api/summer_mode")
-    async def set_summer_mode(self, summer_mode: str) -> str:
+    async def set_summer_mode(self, summer_mode: str) -> dict[str, str]:
         result: bool = await self.summer_mode_repository.set_summer_mode(
             SummerMode.from_mode_string(summer_mode)
         )

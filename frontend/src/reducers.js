7aba456de515d54a76156161a5e0db3ea58b4553
Marcin Goncerzewicz <marcingoncerzewicz@gmail.com>
2024-06-04 06:34:22 +0200

    [ENH] summer mode update reducers

--- frontend/src/reducers.js
+++ frontend/src/reducers.js
@@ -3,10 +3,12 @@ import { combineReducers } from 'redux'
 import temperatureReducer from './features/temperature/temepratureSlice'
 import stoveStateReducer from './features/stoveState/stoveStateSlice'
 import flameSizeReducer from './features/flameSize/flameSizeSlice'
+import summerModeReducer from './features/summerMode/summerModeSlice'
 const rootReducer = combineReducers({
   temperature: temperatureReducer,
   stoveState: stoveStateReducer,
   flameSize: flameSizeReducer,
+  summerMode: summerModeReducer,
 })
 
 export default rootReducer

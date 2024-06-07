20385afd232cc6f0b8fd87cefee64f54d079ac3e
Marcin Goncerzewicz <marcingoncerzewicz@gmail.com>
2024-06-08 00:10:17 +0200

    [ENH] Summer mode change slice

--- frontend/src/features/summerMode/summerModeSlice.js
+++ frontend/src/features/summerMode/summerModeSlice.js
@@ -1,12 +1,20 @@
-import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import {createAsyncThunk, createEntityAdapter, createSlice} from "@reduxjs/toolkit";
 import {client} from "../../api/client";
+import {setHeatingTemperature} from "../temperature/temepratureSlice";
 
 const initialState = {
     summerMode: undefined,
 }
 
+const summerModeAdapter = createEntityAdapter()
+
 export const fetchSummerMode = createAsyncThunk('summerMode/fetchSummerMode', async () => {
     const response = await client.get('/api/summer_mode')
+    return response
+})
+
+export const setSummerMode = createAsyncThunk('summerMode/setSummerMode', async (summerMode) => {
+    const response = await client.post('/api/summer_mode', {summerMode: summerMode})
     return response
 })
 
@@ -14,7 +22,12 @@ const summerModeSlice = createSlice({
     name: 'summerMode',
     initialState,
     reducers: {
-
+      updateSummerMode(state, action) {
+        const newSummerMode = action.payload
+        if (newSummerMode !== undefined) {
+          state.entities.summerMode = newSummerMode
+        }
+      },
     },
     extraReducers: builder => {
         builder
@@ -27,6 +40,14 @@ const summerModeSlice = createSlice({
                 state.summerMode = summerMode
                 state.status = 'idle'
             })
+            .addCase(setSummerMode.pending, (state, action) => {
+                state.status = 'loading'
+            })
+            .addCase(setHeatingTemperature.fulfilled, (state, action) => {
+                const summerMode = {}
+                summerMode.summerMode = action.payload.summer_mode
+                state.summerMode = summerMode
+              })
     }
 });
 
